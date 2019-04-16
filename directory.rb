5caf38ac054d756269aa29bf31608dab90a1f774
EC2 Default User <ec2-user@ip-172-31-39-174.us-east-2.compute.internal>
2019-04-16 21:45:25 +0000

    add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,31 @@
+def interactive_menu
+  students = []
+  # 4. repeat from step 1
+  loop do
+  # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+  # 2. read the input and save it into a variable
+    selection = gets.chomp
+  # 3. do what the user has asked
+    case selection
+      when "1"
+        # input the students
+        students = input_students
+      when "2"
+        # show the students
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -31,8 +59,5 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-# nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+interactive_menu
