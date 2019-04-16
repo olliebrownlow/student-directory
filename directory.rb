0760eec2d468ab73249003e5e7b45897777b3cba
EC2 Default User <ec2-user@ip-172-31-39-174.us-east-2.compute.internal>
2019-04-16 22:41:35 +0000

    add function to save to file

--- directory.rb
+++ directory.rb
@@ -5,15 +5,27 @@ def interactive_menu
   loop do
   # 1. print the menu and ask the user what to do
     print_menu
-  # 2. read the input and save it into a variable
-    process(gets.chomp)
   # 3. do what the user has asked
+    process(gets.chomp)
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -31,6 +43,8 @@ def process(selection)
     when "2"
       # show the students
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
