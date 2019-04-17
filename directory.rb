d1e4ee0d2cb0c09947e1285e0715a2da32bf6440
EC2 Default User <ec2-user@ip-172-31-39-174.us-east-2.compute.internal>
2019-04-17 22:08:38 +0000

    add function to load file from command line argument

--- directory.rb
+++ directory.rb
@@ -6,7 +6,7 @@ def interactive_menu
   # 1. print the menu and ask the user what to do
     print_menu
   # 3. do what the user has asked
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -22,9 +22,21 @@ def save_students
   file.close
 end
 
-def load_students
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} students from #{filename}"
+  else 
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
+def load_students(filename = "students.csv")
   # open the file for reading
-  file = File.open("students.csv", "r")
+  file = File.open(filename, "r")
   # iterate over each line in the file
   file.readlines.each do |line|
     # parallel assignment
@@ -71,14 +83,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -96,5 +108,7 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+
+try_load_students
 
 interactive_menu
